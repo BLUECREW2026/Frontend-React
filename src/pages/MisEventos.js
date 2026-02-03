93b2d7d2e3f4f8943b743f985736647f49c734cb
Iker <ikerpastorros@gmail.com>
2026-02-03 15:04:13 +0100

    Cambios Pequeños

--- src/pages/MisEventos.js
+++ src/pages/MisEventos.js
@@ -1,8 +1,19 @@
-import datos from '../data/Eventos'
+import { eventos } from '../data/Eventos'
+import GrupoDeCardEventoSmall from '../components/GrupoDeCardEventoSmall';
 
 export default function MisEventos() {
     return (
-        <div>
-        </div>
+        <>
+            <div>
+                <div>
+                    <p>CALIFICADOS</p>
+                </div>
+                <GrupoDeCardEventoSmall datos={eventos} />
+                <div>
+                    <p>PENDIENTES DE CALIFICAR</p>
+                </div>
+                <GrupoDeCardEventoSmall datos={eventos} />
+            </div>
+        </>
     );
 }
