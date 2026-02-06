89004d4652e8ab25599ac7d8a6235524224b8032
Iker <ikerpastorros@gmail.com>
2026-02-06 11:35:31 +0100

    Hecho

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,12 @@
+import FormularioContacto from '../components/FormularioContacto';
+
+function Contacto() {
+    return (
+
+        <>
+            <FormularioContacto></FormularioContacto>
+        </>
+    );
+}
+
+export default Contacto;
