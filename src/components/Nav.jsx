2ef6de4a84a7d42c7fc3c8ff0ad7739b5841d02a
Denys <denyspetrychenko@gmail.com>
2026-02-21 13:05:36 +0100

    Realizadas mejoras de funcionamiento en el Nav. Implementada funcionalidad de Navegación y problema con OffCanvas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,43 @@
 import Logo from "../assets/logo/bluecrew-logo-complete.svg";
-import LogoDos from "../assets/logo/bluecrew-logo-complete-white.svg";
+import LogoBlanco from "../assets/logo/bluecrew-logo-complete-white.svg";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import DesplegableUsuario from "./DesplegableUsuario";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 
 export default function Navbar() {
-  const [isLogged, setIsLogged] = useState(false);
 
+  const [isLogged, setIsLogged] = useState(false);
   const toggleLogin = () => setIsLogged(!isLogged);
+  
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-top">
+      <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
         <div className="container-fluid">
 
+          <Link className="navbar-brand ms-2" to="/">
+            <img src={Logo} height="40" alt="BlueCrew Logo" />
+          </Link>
+
           <button
-            className="navbar-toggler d-md-none"
+            className="navbar-toggler d-md-none border-0"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#menuLateral"
           >
-            <span className="navbar-toggler-icon"></span>
+            <i className="bi bi-list fs-2 text-secondary"></i>
+            {/*<span className="navbar-toggler-icon"></span>*/}
           </button>
 
-          <a className="navbar-brand ms-2" href="#">
-            <img src={Logo} height="40" />
-          </a>
-
           <div className="d-none d-md-flex ms-auto">
-            <ul className="navbar-nav fw-bold align-items-center">
-              <li className="nav-item"><a className="nav-link text-secondary" href="#">Inicio</a></li>
-              <li className="nav-item"><a className="nav-link text-secondary" href="#">Noticias</a></li>
-              <li className="nav-item"><a className="nav-link text-secondary" href="#">Sobre Nosotros</a></li>
-              <li className="nav-item"><a className="nav-link text-secondary" href="#">Eventos</a></li>
-             <li className="nav-item ms-3">
+            <ul className="navbar-nav fw-bold align-items-center gap-4">
+              <li className="nav-item"><NavLink className="nav-link text-secondary" to="/">Inicio</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link text-secondary" to="/noticias">Noticias</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link text-secondary" to="/sobre-nosotros">Sobre Nosotros</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link text-secondary" to="/eventos">Eventos</NavLink></li>
+             
+              <li className="nav-item ms-3">
                 {isLogged ? (
                   <DesplegableUsuario onLogout={toggleLogin}/>
                 ) : (
@@ -45,22 +48,23 @@ export default function Navbar() {
               </li>
             </ul>
           </div>
-
         </div>
       </nav>
 
-      <div className="offcanvas offcanvas-start bg-secondary d-md-none" id="menuLateral">
+      {/* Menú lateral para móviles (Offcanvas) */}
+      <div className="offcanvas offcanvas-end bg-secondary d-md-none" id="menuLateral">
         <div className="offcanvas-header">
-          <img src={LogoDos} height="40" />
+          <img src={LogoBlanco} height="40" alt="BlueCrew Logo Blanco" />
           <button className="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
         </div>
 
         <div className="offcanvas-body">
           <ul className="nav flex-column fs-5 gap-3">
-            <li><a className="nav-link text-white" href="#">Inicio</a></li>
-            <li><a className="nav-link text-white" href="#">Noticias</a></li>
-            <li><a className="nav-link text-white" href="#">Sobre Nosotros</a></li>
-            <li><a className="nav-link text-white" href="#">Eventos</a></li>
+            <li><NavLink className="nav-link text-white" to="/">Inicio</NavLink></li>
+            <li><NavLink className="nav-link text-white" to="/noticias">Noticias</NavLink></li>
+            <li><NavLink className="nav-link text-white" to="/sobre-nosotros">Sobre Nosotros</NavLink></li>
+            <li><NavLink className="nav-link text-white" to="/eventos">Eventos</NavLink></li>
+            
             <li className="nav-item">
               {isLogged ? (
                 <>
@@ -77,14 +81,14 @@ export default function Navbar() {
                   
                   <div className="collapse" id="menuUsuario"> 
                     <ul className="nav flex-column ms-3 fs-6 gap-2 mt-2">
-                      <li><Link className="nav-link text-white-50" to="/perfil">Mi Cuenta</Link></li>
-                      <li><Link className="nav-link text-white-50" to="mis-eventos">Mis Eventos</Link></li>
-                       <li><Link className="nav-link text-danger" to="/" onClick={toggleLogin}>Mis Eventos</Link></li>
+                      <li><Link className="nav-link text-white-50" to="/perfil" data-bs-dismiss="offcanvas">Mi Cuenta</Link></li>
+                      <li><Link className="nav-link text-white-50" to="mis-eventos" data-bs-dismiss="offcanvas">Mis Eventos</Link></li>
+                      <li><Link className="nav-link text-danger" to="/" onClick={toggleLogin} data-bs-dismiss="offcanvas">Cerrar Sesión</Link></li>
                     </ul>
                   </div>
                 </>
               ) : (
-                <a className="btn accent-button fw-bold w-100 mt-4" href="#" onClick={toggleLogin}>Únete</a>
+                <Link className="btn accent-button fw-bold w-100 mt-4" to="/login" onClick={toggleLogin}>Únete</Link>
               )}
             </li>
           </ul> 
