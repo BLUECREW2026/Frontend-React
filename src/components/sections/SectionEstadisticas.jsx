5e56333db5d8e3eab7ecda73cdd8efa6a3d234ca
Iker <ikerpastorros@gmail.com>
2026-03-06 13:56:07 +0100

    Cambios generales en className añadir axios

--- src/components/sections/SectionEstadisticas.jsx
+++ src/components/sections/SectionEstadisticas.jsx
@@ -1,23 +1,46 @@
+import clienteAxios from "../../config/axios"
+import { useState, useEffect } from "react";
+
 export default function Estadisticas() {
+
+  const [datos, setDatos] = useState({
+    Voluntarios_Activos: 0,
+    Eventos_Finalizados: 0,
+    Total_Basura: 0
+  })
+
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const response = await clienteAxios.get('/estadisticas/globales');
+        setDatos(response.data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    obtenerDatos();
+  }, []);
+
   return (
     <section>
-      <div class="container text-center py-5 text-secondary">
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          <div class="col">
-            <i class="bi bi-person-circle fs-1"></i>
-            <h3 class="fw-bold mt-2">421.768</h3>
+      <div className="container text-center py-5 text-secondary">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <div className="col">
+            <i className="bi bi-person-circle fs-1"></i>
+            <h3 className="fw-bold mt-2">{datos.Voluntarios_Activos}</h3>
             <p>Voluntarios Activos</p>
           </div>
 
-          <div class="col">
-            <i class="bi bi-calendar4 fs-1"></i>
-            <h3 class="fw-bold mt-2">7135</h3>
+          <div className="col">
+            <i className="bi bi-calendar4 fs-1"></i>
+            <h3 className="fw-bold mt-2">{datos.Eventos_Finalizados}</h3>
             <p>Eventos Realizados</p>
           </div>
 
-          <div class="col">
-            <i class="bi bi-recycle fs-1"></i>
-            <h3 class="fw-bold mt-2">32567 Kg</h3>
+          <div className="col">
+            <i className="bi bi-recycle fs-1"></i>
+            <h3 className="fw-bold mt-2">{datos.Total_Basura} kg</h3>
             <p>Residuos Recogidos</p>
           </div>
         </div>
