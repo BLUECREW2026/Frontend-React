aa34bc2964adac555f1c8d4fd2ffce2e3cfd5b11
Malory Caicedo <malory.caicedo@iesdoctorbalmis.com>
2026-03-09 20:32:10 +0100

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,12 +42,39 @@ function Home() {
     obtenerDatos();
   }, []);
 
+  const importarTodo = (r) => r.keys().map(r);
+  const logos = importarTodo(
+    require.context("/src/assets/ong", false, /\.(png|jpe?g|svg)$/),
+  );
+
   return (
     <div className="flex-grow-1">
       <HeroSection />
       <SectionEstadisticas />
       <GrupoDeCardEvento datos={datos.slice(0, 4)} />
       <SectionONG />
+
+      <div className="colaboradores-section py-5 position-relative">
+        <div className="container-fluid text-center overflow-hidden">
+          <h3 className="text-white mb-5 fw-normal">COLABORAMOS CON:</h3>
+
+          <div className="slider-logos">
+            <div className="slide-track">
+              {logos.map((logo, index) => (
+                <div className="slide" key={`logo-original-${index}`}>
+                  <img src={logo} alt={`Colaborador ${index}`} />
+                </div>
+              ))}
+
+              {logos.map((logo, index) => (
+                <div className="slide" key={`logo-duplicado-${index}`}>
+                  <img src={logo} alt={`Colaborador ${index}`} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
