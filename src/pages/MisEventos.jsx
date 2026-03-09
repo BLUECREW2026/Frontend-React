45526daa13ba653f42e17ca293f016c3e4d533d9
joel barreira <joebarmet@alu.edu.gva.es>
2026-03-09 14:43:48 +0100

    sacoDatosMisEventos

--- src/pages/MisEventos.jsx
+++ src/pages/MisEventos.jsx
@@ -1,20 +1,56 @@
+import { useState, useEffect } from "react";
+import clienteAxios from "../config/axios";
 import GrupoDeCardEventoSmall from "../components/cards/GrupoDeCardEventoSmall";
 
-export default function MisEventos({ datos }) {
+export default function MisEventos() {
+  const [eventos, setEventos] = useState([]);
+
+  const usuarioLogueadoId = 2;
+
+  useEffect(() => {
+    const fetchEventos = async () => {
+      try {
+        const response = await clienteAxios.get("/eventos");
+        setEventos(response.data);
+      } catch (error) {
+        console.error("Error al obtener los eventos:", error);
+      }
+    };
+
+    fetchEventos();
+  }, []);
+
+  
+  const publicados = eventos.filter(
+    (ev) => ev.usuario.id === usuarioLogueadoId
+  );
+
+  const pendientes = eventos.filter((ev) => ev.usuario.id === usuarioLogueadoId && ev.estadoEvento === "PENDIENTE");
+
   return (
-    <div className="flex-grow-1 mt-5">
-      <div className="text-center">
-        <p className="text-secondary h2">INSCRITO</p>
-      </div>
-      <GrupoDeCardEventoSmall datos={datos} />
-      <div className="text-center">
-        <p className="text-secondary h2">PUBLICADOS</p>
-      </div>
-      <GrupoDeCardEventoSmall datos={datos} />
-       <div className="text-center">
-        <p className="text-secondary h2">PENDIENTES DE APROBACIÓN</p>
-      </div>
-      <GrupoDeCardEventoSmall datos={datos} />
+    <div className="flex-grow-1 mt-5 container">
+      <section className="mb-5">
+        <div className="text-center mb-4">
+          <p className="text-secondary h2 fw-bold">INSCRITO</p>
+          <hr className="w-25 mx-auto" />
+        </div>
+      </section>
+
+      <section className="mb-5">
+        <div className="text-center mb-4">
+          <p className="text-secondary h2 fw-bold">MIS EVENTOS PUBLICADOS</p>
+          <hr className="w-25 mx-auto" />
+        </div>
+        <GrupoDeCardEventoSmall datos={publicados} />
+      </section>
+
+      <section className="mb-5">
+        <div className="text-center mb-4">
+          <p className="text-secondary h2 fw-bold">PENDIENTES DE APROBACIÓN</p>
+          <hr className="w-25 mx-auto" />
+        </div>
+        <GrupoDeCardEventoSmall datos={pendientes} />
+      </section>
     </div>
   );
 }
