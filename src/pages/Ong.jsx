c31f1d516f7db6ddcd3233c59493cb6f9780bc8b
Malory Caicedo <malory.caicedo@iesdoctorbalmis.com>
2026-03-09 21:19:22 +0100

    carrucel convertido a componente

--- src/pages/Ong.jsx
+++ src/pages/Ong.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/sections/ong/HeroOng";
-import Carrucel from "../components/sections/ong/InfOng";
+import Info from "../components/sections/ong/InfOng";
+import Carrucel from "../components/common/carrucel";
 
 export default function Ong() {
     return (
         <div className="flex-grow-1">
             <Hero />
+            <Info />
             <Carrucel />
         </div>
     );
